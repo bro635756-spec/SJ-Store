50df716923f7a00d49507a5a468002cc3b276305
bro635756-spec <bro635756@gmail.com>
2026-03-09 16:27:31 +0300

    Delete kotlin_builders.js

--- kotlin_builders.js
+++
@@ -1,6 +0,0 @@
-// Arayüz oluşturucu yardımcı fonksiyonlar
-function filterCat(category) {
-    if(category === 'Hepsi') return displayApps(appData);
-    displayApps(appData.filter(a => a.cat === category));
-}
-console.log("Kotlin Builders yüklendi.");
